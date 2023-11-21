2b88b3049ec29ecba6c7e9f374c4cae2bda4fd4b
Michael DENAHOU <michaeldenahou@gmail.com>
2023-11-21 10:18:41 +0100

    ff

--- src/main/java/springboot/pdl/pdl/projet/repository/FileRepository.java
+++ src/main/java/springboot/pdl/pdl/projet/repository/FileRepository.java
@@ -3,8 +3,6 @@ package springboot.pdl.pdl.projet.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import springboot.pdl.pdl.projet.models.Files;
 
-import java.util.Optional;
-
 public interface FileRepository extends JpaRepository<Files, Long> {
     // Optional<Files> findByNom(String filename);
     // Optional<Files> findByPathFile(String path_file);
